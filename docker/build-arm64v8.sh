6c46506de017765498d771713e6ad413ed3552e1
liuxy0551 <liuxy0551@qq.com>
2024-07-03 19:10:34 +0800

    feat: docker arm64v8 and amd64

--- docker/build-arm64v8.sh
+++ docker/build-arm64v8.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+
 cd docker
+
+# arm64/v8
 docker buildx build --platform linux/arm64/v8 -f Dockerfile.mysql -t liuxy0551/yice-mysql .
 docker buildx build --platform linux/arm64/v8 -f Dockerfile.server -t liuxy0551/yice-server ../
-
-# docker-compose up -d
-
