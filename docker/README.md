feeb87b8208530598f81f3b2fdfd60a73196385c
liuxy0551 <liuxy0551@qq.com>
2024-07-08 17:34:19 +0800

    feat: docker done

--- docker/README.md
+++
@@ -1,1 +0,0 @@
-电脑为 macos m1 arm64/v8，部分基础镜像和 x86 的有区别，体现在 Dockerfile 的 FROM 字值
--- docker/build-mysql-arm64v8.sh
+++ docker/build-mysql-arm64v8.sh
@@ -3,4 +3,6 @@
 cd docker
 
 # arm64/v8
-docker buildx build --platform linux/amd64 -f Dockerfile.mysql -t liuxy0551/yice-mysql --build-arg BASE_IMAGE=biarms/mysql:5.7.30-linux-arm64v8 .
+docker buildx build --platform linux/arm64v8 -f Dockerfile.mysql -t liuxy0551/yice-mysql --build-arg BASE_IMAGE=biarms/mysql:5.7.30-linux-arm64v8 .
+
+# `Dockerfile.mysql` 支持构建 `linux/arm64v8` 架构的镜像，执行 `sh docker/build-mysql-arm64v8.sh` 可生成 arm64v8 架构的 mysql 镜像
