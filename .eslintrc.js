85d0a107e6d38d3d64c94937c26c09271ed2099e
liuxy0551 <liuxy0551@qq.com>
2024-07-09 10:41:09 +0800

    ci: use ko-lint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
     extends: [require.resolve('ko-lint-config/.eslintrc')],
     rules: {
-        camelcase: 0,
-        'no-useless-escape': 0,
-        'prettier/prettier': 2,
+        // camelcase: 0,
+        // 'no-useless-escape': 0,
+        // 'prettier/prettier': 2,
         'react-hooks/exhaustive-deps': 0,
     },
 };
